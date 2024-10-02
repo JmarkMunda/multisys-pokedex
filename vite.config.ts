d2945b8fee5586e65d3d0ed3ce9a56252c6c19ef
JmarkMunda <jaymarkmunda26@gmail.com>
2024-10-02 11:28:12 -0700

    build: modify package json and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/multisys-pokedex/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
-
